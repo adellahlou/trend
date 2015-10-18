8f177438f8c5eaa746dad61446a5011dc65dddf6
Adel Lahlou <adellahlou@users.noreply.github.com>
2015-10-18 03:23:17 -0500

    Started cheap refactor. CSS and requests not working

--- app/trends/scripts/IndexController.js
+++ app/trends/scripts/IndexController.js
@@ -1,72 +1,199 @@
+var PreferencesService, RequestsService, SourcesService;
+
 angular
-  .module('trends', ['supersonic'])
-  .controller('IndexController', function($scope, supersonic, $http) {
+.module('trends', ['supersonic'])
+.controller('IndexController', function($scope, supersonic, $http) {
+  	initializeServices($http);
+
+  	 // initialize the news sources with their properties for css and selection set at a default.
+    $scope.news = SourcesService.getSources();
+
+  	// Will return to trends home-view.
+	$scope.cancel = function () { $scope.focus = false; }
+
   	// initializing trends from twitter/trends
-		$http.get("http://secret-mesa-1979.herokuapp.com/twitter/trends")
-			.then(function(data){
-				$scope.trends = data.data[0].trends;
-	    });
-    // initialize the news sources with their properties for css and selection set at a default.
-    $scope.news = ["Twitter","9GAG","Bing","Google"].map(function(item){
-    	if (item == "9GAG") {
-    		return {"name": item, "css": "GAG", "selected": false}
-    	} else if (item == "New York Times") {
-    		return {"name": item, "css": "NYT", "selected": false}
-    	}else {
-	    	return {"name": item, "css": item, "selected": false}
-	    }
+	$http.get("http://secret-mesa-1979.herokuapp.com/twitter/trends")
+		.then(function(data){
+			$scope.trends = data.data[0].trends;
     });
-    // ["Reddit","Twitter","Buzzfeed","9GAG","Facebook","Google","Yahoo","New York Times"]
-    // function for adding news source to search function
-		$scope.addNews = function (src) {
-			src.selected = !src.selected;
+	// RequestsService.getTrends(function(data){ 
+	// 	$scope.trends = data.data[0].trends;
+ //    });
+   
+	// function for adding news source to search function
+	$scope.addNews = SourcesService.toggleSource;
+
+
+	// ev is a search term. Function will split on # if it exists if not append search to url.
+	$scope.find = function (ev) {
+		if (typeof ev == 'undefined' || !ev.length) {
+			supersonic.ui.dialog.alert("Please enter a search");
+		} 
+
+		$scope.showSpinner = true;
+		var search = ev.replace('#', '');
+		
+		if ($scope.focus) 
+			var request = {sites: SourcesService.getSelectedSources().join(','), search: search};
+		else 
+			var request = {sites: "twitter,google,ninegag,bing", search: search};
+
+		if (!request.sites.length) {
+			$scope.showSpinner = false;
+			supersonic.ui.dialog.alert("No sources selected");
+		} else {
+			RequestsService.searchSites(request, function(data){
+				localStorage.setItem("data", JSON.stringify(data.data));
+				$scope.showSpinner = false;
+				var view = new supersonic.ui.View("trends#posts");
+			  supersonic.ui.layers.push(view);
+			});
 		}
-		// ev is a search term. Function will split on # if it exists if not append search to url.
-		$scope.find = function (ev) {
-			if (typeof ev == 'undefined' || !ev.length) {
-				supersonic.ui.dialog.alert("Please enter a search");
-			} else {
-				$scope.showSpinner = true;
-				var name = ev.split("#");
-				if (name.length == 2){
-					var search = encodeURI(name[1]);
-				} else {
-					var search = encodeURI(name[0]);
-				}
-				var url = "http://secret-mesa-1979.herokuapp.com/search";
-				if ($scope.focus) {
-					var request = {sites: "", search: search};
-					$scope.news.forEach(function(item){
-						if (item.name == "Twitter" && item.selected){
-							request.sites += "twitter,";
-						} else if (item.name == "Google" && item.selected){
-							request.sites += "google,";
-						} else if (item.name == "9GAG" && item.selected){
-							request.sites += "ninegag,";
-						} else if (item.name == "Bing" && item.selected){
-							request.sites += "bing,";
-						}
-					});
-					request.sites = request.sites.length ? request.sites.slice(0,request.sites.length - 1) : request.sites;
-				} else {
-					var request = {sites: "twitter,google,ninegag,bing", search: search};
-				}
-				if (!request.sites.length) {
-					$scope.showSpinner = false;
-					supersonic.ui.dialog.alert("No sources selected");
-				} else {
-					$http.post(url, request)
-					.then(function(data){
-						localStorage.setItem("data", JSON.stringify(data.data));
-						$scope.showSpinner = false;
-						var view = new supersonic.ui.View("trends#posts");
-					  supersonic.ui.layers.push(view);
-					});
-				}
+	}
+	
+	
+});
+
+
+function initializeServices($http){
+	PreferencesService = (function(){
+		var settings = {};
+		var baseUrl = "http://secret-mesa-1979.herokuapp.com";
+
+
+		function loginAndGetPreferences(user, pass, cb){
+			$http.get(baseUrl + '/user/login')
+				.then(cb);
+		}
+
+
+		function savePreferences(cb){
+			$http.get(baseUrl + '/user/save?' + settings.user.token)
+				.then(cb)
+		}
+
+		return {
+			loginAndGetPreferences : loginAndGetPreferences,
+			savePreferences : savePreferences
+		};
+	})();
+
+
+	RequestsService = (function(){
+		var baseUrl = "http://secret-mesa-1979.herokuapp.com";
+
+		/**
+		*
+		*/
+		function getTrends(request, cb){ 
+			// defaults to global trends
+			if (request){
+				var trendUrl = baseUrl + (request.default ?  '/twitter/trends' : '/search');
+
+				$http.get(trendUrl, request)
+				.then(cb)
 			}
+		}//end getTrends
+
+
+		/**
+		*
+		*/
+		function searchSites(request, cb){
+			if (!request === 0)
+				return {};
+
+			var searchUrl = baseUrl + '/search';
+
+			$http.post(searchUrl, request)
+				.then(cb);
+		} // end searchSite
+	})();
+
+
+
+	SourcesService = (function(){
+		//default values
+		var sites = {
+			twitter: {
+				display : 'Twitter',
+				value : 'twitter',
+				selected : false,
+			}, 
+			ninegag: {
+				display : '9gag',
+				value : 'ninegag',
+				selected : false,
+			}, 
+			nyt : {
+				display : 'NYT',
+				value : 'nyt',
+				selected : false,
+			} , 
+			bing : {
+				display : 'Bing',
+				value : 'bing',
+				selected : false,
+			}, 
+			google : {
+				display : 'Google',
+				value : 'google',
+				selected : false,
+			}, 
+			npr : {
+				display : 'NPR',
+				value : 'npr',
+				selected : false,
+			}
+		};
+
+
+		// function setSelectedSources(srcs){
+		// 	if(typeof srcs === 'object'){
+
+		// 	} else if(typeof srcs ==='array')
+		// }
+
+		function toggleSource(src){
+			sites[src.value].selected = !sites[src.value].selected;
 		}
-		// Will return to trends home-view.
-		$scope.cancel = function () {
-			$scope.focus = false;
+
+		function getSources(){
+			var siteKeys = Object.keys(sites);
+			var ret = [];
+			siteKeys.forEach(function(key){
+				ret.push(sites[key]);
+			})
+
+			return ret;
 		}
-  });
+
+		function getSelectedSources(){
+			var siteKeys = Object.keys(sites);
+			var ret = [];
+			for(key in siteKeys){
+				if(sites[key].selected)
+					ret.push(sites[key].value);
+			}
+
+			return ret;
+		}
+
+		return {
+			getSelectedSources : getSelectedSources,
+			getSources: getSources,
+			// setSelectedSources : setSelectedSources,
+			toggleSource : toggleSource
+		};
+	})();	
+
+};
+
+
+
+
+
+
+
+
+
